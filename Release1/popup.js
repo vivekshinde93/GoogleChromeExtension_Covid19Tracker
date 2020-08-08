6361c0848a941816ec6340880612fd52d6fd0e79
vivekshinde93 <projectbluevivek@gmail.com>
2020-08-08 16:07:48 +0530

    Adding Release 1

---
+++ Release1/popup.js
@@ -0,0 +1,9 @@
+var name = "";
+var click =document.getElementById('enter');
+getElementById('nameField').oninput = function(){this.name};
+click.onclick = function (){
+    document.getElementById('Container').style.display='none';
+    document.getElementById('nameDisplay').style.display='block';
+    document.getElementById('nameDisplay').appendChild= $name;
+
+}
